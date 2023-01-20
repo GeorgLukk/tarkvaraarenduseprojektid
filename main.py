fbdec44b28be09036bdcf9396371fd20195c1f23
georg.lukk <georg.lukk@vikk.ee>
2023-01-20 13:17:56 +0200

    test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 #lisa 1
 #alustab koodi ja impordib selle mooduli
 import pygame
@@ -18,6 +19,98 @@ pygame.draw.polygon(screen, [3, 3, 3], [[80,410],[210,410],[230,430],[60,430],[8
 pygame.draw.polygon(screen, [255, 255, 255], [[60,430],[230,430],[250,450],[40,450],[60,430]], 0)
 pygame.display.flip()#värskendab akna
 #hoiab akna lahti
+running = True
+while running:#teeb tsükli
+    for event in pygame.event.get():#kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT:#kui programm pannakse kinni läheb ka pygame kinni
+            running == False
+'''
+'''
+#tekstide ja piltide kasutamine
+import pygame
+pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])#annab resulutsiooni loodud aknale
+pygame.display.set_caption("Harjutamine")#annab aknale nime
+screen.fill([204, 255, 204])#paneb taustavärvi
+#lisame teksti
+#font = pygame.font.SysFont("Tahoma", 50)
+font = pygame.font.Font(pygame.font.match_font('arial'), 50)
+font = pygame.font.Font(None, 30)#annab testile fondi ning selle suuruse
+text = font.render("Hello PyGame", True, [0,0,0])#kirjutab selle lause ning annab selle värvi
+screen.blit(text, [220,220])#vaatab mis koodrinaatidele on see tekst kirjutatud
+pygame.display.flip()#värskendab lehte
+#hoiab akna lahti
+running = True
+while running:#teeb tsükli
+    for event in pygame.event.get():#kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT:#kui programm pannakse kinni läheb ka pygame kinni
+            running == False
+'''
+'''
+import pygame
+pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])#annab resulutsiooni loodud aknale
+pygame.display.set_caption("Harjutamine")#annab aknale nime
+screen.fill([204, 255, 204])#paneb taustavärvi
+#lisame teksti
+font = pygame.font.Font(pygame.font.match_font('arial'), 50)#annab tekstile uue fondi
+font.set_underline(True)#lisab alumise joone
+text = font.render("Hello PyGame", True, [0,0,0])#kirjutab selle lause ja vaatab mis see värv on
+#tekstikasti suurus
+text_width = text.get_rect().width#annab tekstikastile laiuse
+text_height = text.get_rect().height#annab tekstikastile laiuse
+screen.blit(text, [320-text_width/2,240-text_height/2])
+pygame.display.flip()
+running = True
+while running:#teeb tsükli
+    for event in pygame.event.get():#kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT:#kui programm pannakse kinni läheb ka pygame kinni
+            running == False
+'''
+'''
+import pygame
+pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])#annab resulutsiooni loodud aknale
+pygame.display.set_caption("Harjutamine")#annab aknale nime
+screen.fill([204, 255, 204])#paneb taustavärvi
+#Lisame pildid
+bg = pygame.image.load("img/pilt.jpg")
+screen.blit(bg,[0,0])
+
+pygame.display.flip()
+running = True
+while running:#teeb tsükli
+    for event in pygame.event.get():#kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT:#kui programm pannakse kinni läheb ka pygame kinni
+            running == False
+'''
+import pygame
+pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])#annab resulutsiooni loodud aknale
+pygame.display.set_caption("Ülessanne 2")#annab aknale nime
+screen.fill([204, 255, 204])#paneb taustavärvi
+#Lisame pildid
+bg_shop = pygame.image.load("img/bg_shop.jpg")
+seller = pygame.image.load("img/seller.png")
+chat = pygame.image.load("img/chat.png")
+#annme piltidele suurused
+bg_shop = pygame.transform.scale(bg_shop, [640, 480])
+seller = pygame.transform.scale(seller, [300, 240])
+chat = pygame.transform.scale(chat, [200, 100])
+#anname pilltidele asukoha koordinaadid
+screen.blit(bg_shop,[0,0])
+screen.blit(seller,[140,180])
+screen.blit(chat,[340,150])
+pygame.display.flip()#värskendab akent
+font = pygame.font.Font(None, 50)#annab testile fondi ning selle suuruse
+text = font.render("Georg", True, [204,20,40])#kirjutab selle lause ning annab selle värvi
+screen.blit(text, [380,180])#vaatab mis koodrinaatidele on see tekst kirjutatud
+pygame.display.flip()#värskendab akent
+#alustab tsükli
 running = True
 while running:#teeb tsükli
     for event in pygame.event.get():#kui alustatakse programm siis alustatakse pygame
