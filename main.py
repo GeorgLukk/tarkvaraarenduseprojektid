7deeef7697f81db6b0eb51742584434d8ab60746
georg.lukk <georg.lukk@vikk.ee>
2023-01-27 13:57:10 +0200

    test

--- main.py
+++ main.py
@@ -255,29 +255,3 @@ while run: #teeb tsükli
             run = False
 pygame.quit()
 '''
-import pygame
-import colorsys
-screen = pygame.display.set_mode([640, 480])
-pygame.display.set_caption("Harjutamine")
-
-
-GREEN = [153, 255, 153]
-RED = [255, 0, 0]
-screen.fill(RED)
-y = 1
-for i in range(100):
-    x = 1
-    for i in range(100):
-        pygame.draw.rect(screen, GREEN, [x, y, 15, 15])
-        x += 18
-    y += 18
-
-
-pygame.display.flip()
-
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-pygame.quit()
