ae59eaf25a9367a8a5ecfbf95ddc84a80c5cfd46
georg.lukk <georg.lukk@vikk.ee>
2023-02-07 09:17:37 +0200

    test

---
+++ järjendid ja for-tsükkel konspekt.py
@@ -0,0 +1,96 @@
+'''
+number = [ 0, 'üks', 2, 'kolm']#järjendid võivad olla erinevat tüüpi
+print(number[1])
+tühi_järjend = []#võib olla ka tühi järjend
+numbrid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]#numbritega näide järjendist
+#pythonis algab indekseerimine 0'st
+list = ["Tere", "headaega"]
+print (list[0])
+nädalapäevad = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"]
+#nädalapäevad = ["esmaspäev"#0, "teisipäev"#1, "kolmapäev"#2, "neljapäev"#3, "reede"#4, "laupäev"#5, "pühapäev"#6]
+nimi = ['Malle', 'Volli', 'Salme', 'Kustav']#tekitatakse loend nimedest
+print (nimi[0])#tahetakse väljastada nime mille indeks on 0 ehk Malle
+#SPLIT JA JOIN meetodid
+#split poolitab lause kaheks sealt kus on vahe sees(võib ka defineerida sulgudes millest poolitada)
+'Tere hommikust'.split()
+['Tere', 'hommikust']
+#Join alguses defineeritakse kust neid liidetakse ja siis antakse sulgudesse sõnad mida liita
+' '.join(['Tere', 'hommikust'])
+'Tere hommikust'
+#FOR
+#Näide 1
+for linn in ["Tartu", "Tallinn", "Põltsamaa"]:
+    print("Muutuja linn väärtus: " + linn)
+#prindidakse esimene "" + esimene , teine , kolmas
+#Näide 2
+a = [12, 23, 34, 45, 56]
+i = 0
+for el in a:
+    if el > 30:
+        i += 1
+print(i)
+#prinditakse 12 siis 23 jne kuna seal koodis on i+= 1 siis liidedakse i'le antud indeksile 1 juurde
+
+#FOR RANGE
+#For range varjandid
+list(range(5))
+[0, 1, 2, 3, 4]
+print(list(range(5)))
+print(list(range(0,5)))
+list(range(0, 15, 2))
+[0, 2, 4, 6, 8, 10, 12, 14]
+print(list(range(0,15,2)))#annab sellest range funktsioonist ainult paarisarvud
+list(range(5, 0, -1))
+[5, 4, 3, 2, 1]
+print(list(range(5, 0, -1)))
+for i1 in range(5):#teeb endale range funktsiooni nimega i
+    print(i1)#prindib funksiooni range i 0,1,2,3,4
+for i2 in range(1,5,1):#teeb range funktsiooni 1st 4ni
+    print(i2)#prindib selle range funktsiooni 1,2,3,4
+'''
+'''
+arvud = [2,4,6,7,2]#tehakse järjend nimega arvud
+for i in range(0,5,1):#for range indeksitega 0-5 ning vahe nende indeksitel on 1
+    print(arvud[i])#prindib arvud järjendist
+#roberti test
+arvud = [2,4,6,7,2,99]#tehakse järjend nimega arvud
+for i in range(0,6,5):#for range indeksitega 0-6 ning vahe nende indeksitel on 6
+    print(arvud[i])#prindib arvud järjendist
+
+#saab ka lisada elemendi järjendi lõppu
+a = []
+a.append(2)
+a = [2,3,4,2,6,]#loob järjendi nimega a
+a[2] = 90
+a.append(5)#loob indeksile uue sisendi
+a[5] = 99#täidab indeksi 5 sisendi
+a.remove(2)#removib ära selle arvu järjendist mitte indeksi
+a.append(6)#loob indeksile uue sisendi
+a[5] = 1#täidab indeksi 6 sisendi
+#a.sort()
+print (a)#prindib a järjendi
+'''
+#Sellisel moel saab ka .txt failidest võtta ridu ning väljastada neid python failis
+#1
+f = open('andmed.txt')#teeb .txt faili lahti
+for rida in f:
+    print('Lugesin järgneva rea: ' + rida)
+f.close()#paneb kinni faili
+
+#2
+f = open('nimed.txt')#avab faili
+while True:
+    nimi = f.readline() #loeb failist antud rea
+    if nimi == "":#kui pole seal midagi anda annab see tühja vastuse
+        break
+f.close()#paneb kinni faili
+
+#näide failist lugemisel
+import random#impordib randon funsiooni
+nimekiri = []#teeb tühja järjendi
+f = open('nimekiri_10it.txt',encoding='utf-8')#lisatakse fail ning selle nimi ja kooding
+for rida in f:
+    nimekiri.append(rida.strip())#hakatakse faili lugema iga rida
+f.close()#suleb faili
+suvaline = random.randint(0,len(nimekiri)- 1)#loob suvalise'le enda range'le
+print('suvaline asi on: '+nimekiri[suvaline])#prindib suvalise'le antud rantgest midagi
---
+++ pygame.py
@@ -0,0 +1,272 @@
+'''
+#lisa 1
+#alustab koodi ja impordib selle mooduli
+import pygame
+pygame.init()
+#teeb akna
+screen=pygame.display.set_mode([300,500])
+pygame.display.set_caption("Valgusfoor- Georg")#annab aknale nime
+#kujundite joonistamine
+pygame.draw.rect(screen, [222, 222, 222], [90, 10, 130, 270], 2)#joonistab ristküliku
+pygame.draw.circle(screen, [0, 255, 11], [155,60], 40, 0)#joonistab esimese ringi ja täidab selle värviga
+#pygame.draw.circle(screen, 1.([0, 255, 11]), 2.([155,60]), 3.(40), 4.(0))=1.[Värvi rgb kood] 2.[koordinaadid] 3.ringi raadius 4.0 on seest täitmine, 2 on tühi
+pygame.draw.circle(screen, [255, 246, 0], [155,145], 40, 0)#joonistab teise ringi ja täidab selle värviga
+pygame.draw.circle(screen, [255, 45, 0], [155,230], 40, 0)#joonistab kolmanda ringi ja täidab selle värviga
+pygame.draw.line(screen, [222,222,222], [155,280], [155,390], 2) #teeb posti
+#hakkab seda alust joonistama
+#pygame.draw.polygon(screen, [värv], [[koordinaadid 1],[koordinaadid 2],[koordinaadid 3],[koordinaadid 4],[koordinaadid 5]], 2)
+pygame.draw.polygon(screen, [126, 128, 130], [[100,390],[190,390],[250,450],[40,450],[100,390]], 2)
+pygame.draw.polygon(screen, [0, 123, 255], [[130,390],[190,390],[210,410],[80,410],[100,390]], 0)
+pygame.draw.polygon(screen, [3, 3, 3], [[80,410],[210,410],[230,430],[60,430],[80,410]], 0)
+pygame.draw.polygon(screen, [255, 255, 255], [[60,430],[230,430],[250,450],[40,450],[60,430]], 0)
+pygame.display.flip()#värskendab akna
+#hoiab akna lahti
+running = True
+while running:#teeb tsükli
+    for event in pygame.event.get():#kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT:#kui programm pannakse kinni läheb ka pygame kinni
+            running = False
+pygame.quit()
+'''
+'''
+#harjutamine
+#tekstide ja piltide kasutamine
+import pygame
+pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])#annab resulutsiooni loodud aknale
+pygame.display.set_caption("Harjutamine")#annab aknale nime
+screen.fill([204, 255, 204])#paneb taustavärvi
+#lisame teksti
+#font = pygame.font.SysFont("Tahoma", 50)
+font = pygame.font.Font(pygame.font.match_font('arial'), 50)
+font = pygame.font.Font(None, 30)#annab testile fondi ning selle suuruse
+text = font.render("Hello PyGame", True, [0,0,0])#kirjutab selle lause ning annab selle värvi
+screen.blit(text, [220,220])#vaatab mis koodrinaatidele on see tekst kirjutatud
+pygame.display.flip()#värskendab lehte
+#hoiab akna lahti
+running = True
+while running:#teeb tsükli
+    for event in pygame.event.get():#kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT:#kui programm pannakse kinni läheb ka pygame kinni
+            running = False
+pygame.quit()
+'''
+'''
+#Harjutamine
+import pygame
+pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])#annab resulutsiooni loodud aknale
+pygame.display.set_caption("Harjutamine")#annab aknale nime
+screen.fill([204, 255, 204])#paneb taustavärvi
+#lisame teksti
+font = pygame.font.Font(pygame.font.match_font('arial'), 50)#annab tekstile uue fondi
+font.set_underline(True)#lisab alumise joone
+text = font.render("Hello PyGame", True, [0,0,0])#kirjutab selle lause ja vaatab mis see värv on
+#tekstikasti suurus
+text_width = text.get_rect().width#annab tekstikastile laiuse
+text_height = text.get_rect().height#annab tekstikastile laiuse
+screen.blit(text, [320-text_width/2,240-text_height/2])
+pygame.display.flip()
+running = True
+while running:#teeb tsükli
+    for event in pygame.event.get():#kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT:#kui programm pannakse kinni läheb ka pygame kinni
+            running = False
+pygame.quit()
+'''
+'''
+#ülessane 2 harjutamine
+import pygame
+pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])#annab resulutsiooni loodud aknale
+pygame.display.set_caption("Harjutamine")#annab aknale nime
+screen.fill([204, 255, 204])#paneb taustavärvi
+#Lisame pildid
+bg = pygame.image.load("img/pilt.jpg")
+screen.blit(bg,[0,0])
+
+pygame.display.flip()
+running = True
+while running:#teeb tsükli
+    for event in pygame.event.get():#kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT:#kui programm pannakse kinni läheb ka pygame kinni
+            running = False
+pygame.quit()
+'''
+'''
+#Ülessanne 2 tekstide ja piltide kasutamine
+import pygame
+pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])#annab resulutsiooni loodud aknale
+pygame.display.set_caption("Ülessanne 2")#annab aknale nime
+screen.fill([204, 255, 204])#paneb taustavärvi
+#Lisame pildid
+#pead tegema endale directory piltide jaoks kuhu sa pildid laed
+bg_shop = pygame.image.load("img/bg_shop.jpg")
+seller = pygame.image.load("img/seller.png")
+chat = pygame.image.load("img/chat.png")
+#annme piltidele suurused
+bg_shop = pygame.transform.scale(bg_shop, [640, 480])
+seller = pygame.transform.scale(seller, [300, 240])
+chat = pygame.transform.scale(chat, [200, 100])
+#anname pilltidele asukoha koordinaadid
+screen.blit(bg_shop,[0,0])
+screen.blit(seller,[140,180])
+screen.blit(chat,[340,150])
+pygame.display.flip()#värskendab akent
+font = pygame.font.Font(None, 50)#annab testile fondi ning selle suuruse
+text = font.render("Georg", True, [204,20,40])#kirjutab selle lause ning annab selle värvi
+screen.blit(text, [380,180])#vaatab mis koodrinaatidele on see tekst kirjutatud
+pygame.display.flip()#värskendab akent
+#alustab tsükli
+run = True
+while run: #teeb tsükli
+    for event in pygame.event.get(): #kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT: #kui programm pannakse kinni läheb ka pygame kinni
+            run = False
+pygame.quit()
+'''
+'''
+#Ülessanne 2 tekstide ja piltide kasutamine
+import pygame
+pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])#annab resulutsiooni loodud aknale
+pygame.display.set_caption("Ülessanne 2")#annab aknale nime
+screen.fill([204, 255, 204])#paneb taustavärvi
+#Lisame pildid
+#pead tegema endale directory piltide jaoks kuhu sa pildid laed
+bg_shop = pygame.image.load("img/bg_shop.jpg")
+seller = pygame.image.load("img/seller.png")
+chat = pygame.image.load("img/chat.png")
+vikklogo = pygame.image.load("img/vikklogo.png")
+mõõk = pygame.image.load("img/mõõk.png")
+tort = pygame.image.load("img/tort.png")
+#annme piltidele suurused
+bg_shop = pygame.transform.scale(bg_shop, [640, 480])
+seller = pygame.transform.scale(seller, [300, 240])
+chat = pygame.transform.scale(chat, [200, 100])
+vikklogo = pygame.transform.scale(vikklogo, [300, 50])
+mõõk = pygame.transform.scale(mõõk, [100, 100])
+tort = pygame.transform.scale(tort, [100, 100])
+#anname pilltidele asukoha koordinaadid
+screen.blit(bg_shop,[0,0])
+screen.blit(seller,[140,180])
+screen.blit(chat,[340,150])
+screen.blit(vikklogo,[0,0])
+screen.blit(mõõk,[480,180])
+screen.blit(tort,[350,190])
+pygame.display.flip()#värskendab akent
+font = pygame.font.Font(None, 50)#annab testile fondi ning selle suuruse
+text = font.render("Georg", True, [204,20,40])#kirjutab selle lause ning annab selle värvi
+screen.blit(text, [380,180])#vaatab mis koodrinaatidele on see tekst kirjutatud
+pygame.draw.rect(screen, [255, 255, 255], [0, 0, 300, 50], 2)#joonistab vikk logo ümber ristküliku
+pygame.draw.arc(screen,[255,255,255], [240,0,80,50], -3.14/3, 1)#joonistab kaare vikklogo juurde joonistatud risküliku kõrvale
+pygame.display.flip()#värskendab akent
+#alustab tsükli !!!!!!!!!!!!!!!!!!!
+run = True
+while run: #teeb tsükli
+    for event in pygame.event.get(): #kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT: #kui programm pannakse kinni läheb ka pygame kinni
+            run = False
+pygame.quit()
+'''
+
+'''
+#Ülessanne 3 harjutused
+#(1)
+
+import pygame
+import sys
+pygame.init()
+
+#värvid
+lGreen = [153, 255, 153]
+lBlue = [153, 204, 255]
+
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])
+pygame.display.set_caption("Harjutamine")
+screen.fill(lGreen)
+
+gameover = False
+#alsutab tsükli kui mäng ei ole võit siis see väljastab antud pildi
+while not gameover:
+
+    #Lisame pildid
+    youWin = pygame.image.load("img/youwin.png")
+    youWin = pygame.transform.scale(youWin, [300, 120])
+    screen.blit(youWin,[180,100])
+
+    pygame.display.flip()
+
+pygame.quit()
+'''
+'''
+#harjutamine
+#funksioon mis joonistab maja
+def drawHouse(x, y, width, height, screen, color):#see funksioon küsib kahte punkti,laiust,pikkust,eraldavat suurust ja värvi
+    points = [(x, y - ((3 / 4.0) * height)), (x, y), (x + width, y), (x + width, y - (3 / 4.0) * height),
+              (x, y - ((3 / 4.0) * height)), (x + width / 2.0, y - height), (x + width, y - (3 / 4.0) * height)]
+    lineThickness = 2#annab sellele joonele millega maja joonistatakse suurema laiuse
+    pygame.draw.lines(screen, color, False, points, lineThickness)
+'''
+'''
+#harjutamine
+#imporsib erinevad moodulid
+import pygame
+import sys
+import random
+
+pygame.init()
+
+#defineerib värvid
+red = [255, 0, 0]
+green = [0, 255, 0]
+blue = [0, 0, 255]
+pink = [255, 153, 255]
+lGreen = [153, 255, 153]
+
+# ekraani seaded
+screen = pygame.display.set_mode([640, 480])#akna suurus
+pygame.display.set_caption("Harjutamine")#akna nimi
+screen.fill(lGreen)#akna sisene värv
+
+# funktsioon mida seletasin üleval pool juba
+def drawHouse(x, y, width, height, screen, color):#see funksioon küsib kahte punkti,laiust,pikkust,eraldavat suurust ja värvi
+    points = [(x, y - ((3 / 4.0) * height)), (x, y), (x + width, y), (x + width, y - (3 / 4.0) * height),
+              (x, y - ((3 / 4.0) * height)), (x + width / 2.0, y - height), (x + width, y - (3 / 4.0) * height)]
+    lineThickness = 2#annab sellele joonele millega maja joonistatakse suurema laiuse
+    pygame.draw.lines(screen, color, False, points, lineThickness)
+
+# kutsun funktsiooni välja ehk hakkan seda ülemist funksiooni kasutama (funksioon joonistab järgmisel real antud suuruste järgi)
+drawHouse(100, 400, 300, 200, screen, red)
+
+pygame.display.flip()
+#alustab tsükli !!!!!!!!!!!!!!!!!!!
+run = True
+while run: #teeb tsükli
+    for event in pygame.event.get(): #kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT: #kui programm pannakse kinni läheb ka pygame kinni
+            run = False
+pygame.quit()
+'''
+import pygame
+import colorsys
+screen = pygame.display.set_mode([640, 480])#akna suurus
+pygame.display.set_caption("Random ruudud")#akna nimi
+screen.fill([204, 255, 204])#paneb taustavärvi
+pygame.draw.rect(screen, [222, 222, 222], [0, 0, 30, 30], 2)#joonistab ristküliku
+
+pygame.display.flip()#värskendab akna
+#alustab tsükli !!!!!!!!!!!!!!!!!!!
+run = True
+while run: #teeb tsükli
+    for event in pygame.event.get(): #kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT: #kui programm pannakse kinni läheb ka pygame kinni
+            run = False
+pygame.quit()
