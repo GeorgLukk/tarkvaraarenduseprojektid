b3ad5427d9e3489a102edbd5e9bc756534ed2a0b
georg.lukk <georg.lukk@vikk.ee>
2023-02-07 10:38:03 +0200

    test

--- järjend ja for-tsükkel tööleht.py
+++ järjend ja for-tsükkel tööleht.py
@@ -18,6 +18,28 @@ c= a+b
 c.sort()
 print(c)#väljastab järjendi
 
-#ÜL3
-for ik in range (0,7,1):#teeb range linnade järjendile
-    print (linnad[ik])#prindib linnad
+#ÜL4
+import turtle
+i=0
+for i in range (4):
+    turtle.fd(50)
+    turtle.lt(90)
+    i+1
+turtle.exitonclick()
+
+#ÜL5
+from functools import reduce#impordib recude funktsiooni
+d = [3,4,5,2,6]
+def add(a,b):#loob funktsiooni
+	return a + b #liidab a ja b ning läheb algusesse 3+4 ja läheb tagasi 7 ning nii edasi
+the_sum = reduce(add, d)#reduce liidab iga arvu järjest üksteise otsa
+print(the_sum)#väljastab lõpptulemuse
+
+#ÜL6
+e=[2,4,5,6,12,18]
+for paaris in e:#alustab for tsükli mis vaatab e järjendit
+    if paaris % 2 == 0:#kui järjendis number jagub 2'ga siis see prindib selle
+        print(paaris,end=',')#väljastab paaris numbri ning paneb koma nenede vahele
+#end on funksioon mis paneb midagi iga tsükli lõppu (praegusel juhul ,)
+
+#ÜL7
