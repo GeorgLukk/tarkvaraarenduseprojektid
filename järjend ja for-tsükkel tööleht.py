f5a637358cecaf1e9ec1e3e08ebd45d0ebbd47fc
georg.lukk <georg.lukk@vikk.ee>
2023-02-07 09:22:57 +0200

    test

---
+++ järjend ja for-tsükkel tööleht.py
@@ -0,0 +1,6 @@
+Ülesanne 1. Seitse linna I (kohustuslik)
+Koosta järjend, milles on vähemalt seitse Eesti linna ning järjesta need tähestikulisse järjekorda, kasutades selleks vastavat funktsiooni.
+Lisaks väljasta kasutajale lause, kus öeldakse mitu linna on järjendis, kasutades järjendi elementi lugemiseks vastavat funktsiooni.
+linnad=['Tallinn','Tartu','Viljandi','Pärnu','Rakvere','Pärnu','Haapsalu']
+linnad.sort()
+print ('linnad')
